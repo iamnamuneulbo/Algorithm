a914f07ea96c0aae19ad4413fea5f2c98cff27b5
leesk9719 <leesk9719@gmail.com>
2021-10-01 01:09:10 +0900

    8958 OX퀴즈

---
+++ BaekJoon/src/_8958_OXQuiz.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class _8958_OXQuiz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+
+        for (int i = 0; i < cnt; i++) {
+            String input = sc.next();
+            int score = 0;
+            int answerCnt = 0;
+
+            for (int j = 0; j < input.length(); j++) {
+                if (input.charAt(j) == 'O') {
+                    score += ++answerCnt;
+                }
+                else {
+                    answerCnt = 0;
+                }
+            }
+
+            System.out.println(score);
+        }
+    }
+}
