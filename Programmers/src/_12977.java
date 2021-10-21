1160b72e7a0a0ff0bcc28f53832115bc4e1b5d3c
leesk9719 <leesk9719@gmail.com>
2021-10-22 00:05:46 +0900

    12977 에라토스테네스의 체

--- Programmers/src/_12977.java
+++ Programmers/src/_12977.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+
 public class _12977 {
+    static ArrayList<Boolean> primeNumList;
+
     public static void main(String[] args) {
         System.out.println(solution(new int[]{1, 2, 7, 6, 4}));
     }
@@ -6,10 +10,12 @@ public class _12977 {
     public static int solution(int[] nums) {
         int answer = 0;
 
+        getPrimeNum(1000 * 3);
+
         for (int i = 0; i < nums.length - 2; i++) {
             for (int j = i + 1; j < nums.length - 1; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
-                    if (isPrimeNum(nums[i] + nums[j] + nums[k])) {
+                    if (primeNumList.get(nums[i] + nums[j] + nums[k])) {
                         answer++;
                     }
                 }
@@ -31,5 +37,30 @@ public class _12977 {
         }
 
         return true;
+    }
+
+    public static void getPrimeNum(int range) {
+        int end = (int) Math.sqrt(range);
+
+        primeNumList = new ArrayList<>(range + 1);
+
+        primeNumList.add(false); // 0
+        primeNumList.add(false); // 1
+
+        for (int i = 2; i <= range; i++) {
+            primeNumList.add(i, true);
+        }
+
+        for (int i = 4; i <= range; i += 2) {
+            primeNumList.set(i, false);
+        }
+
+        for (int i = 3; i <= end; i += 2) {
+            if (primeNumList.get(i)) {
+                for (int j = i * 2; j <= range; j += i) {
+                    primeNumList.set(j, false);
+                }
+            }
+        }
     }
 }
